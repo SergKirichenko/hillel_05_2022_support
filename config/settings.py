158102df06f8d23e6db74fac6d6dd5b83917ccb3
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-07-17 21:03:40 +0300

    django-project-1.0.6

--- config/settings.py
+++ config/settings.py
@@ -6,20 +6,20 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i=om*cih+vjqe)2rjm5!cs^!+h2od%ek1)1#pqt0$gwce2wqp7"
+SECRET_KEY = os.environ.get("SECRET_KEY", "")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
 ALLOWED_HOSTS = []
 
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from config.api import btc_usd, history, home
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("home/", home),
+    path("btc_usd/", btc_usd),
+    path("history/", history),
 ]
