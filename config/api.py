864dfa80afa39d9d66c23f5671c252550020c009
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-07-20 20:41:07 +0300

    django-project-1.0.8

--- config/api.py
+++ config/api.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+import json
+import os
 from dataclasses import asdict, dataclass
 from typing import List
 
+import dotenv
 import requests
 from django.http import JsonResponse
 
-# import json
 # from django.http import HttpResponse
 # from http import HTTPStatus
 # def home(request):
@@ -15,6 +17,7 @@ from django.http import JsonResponse
 #     data = {"message": "hello"}
 #     message = json.dumps(data)
 #     return HttpResponse(message, headers=headers, status=HTTPStatus.OK)
+dotenv.read_dotenv()
 
 
 def home(request):
@@ -45,31 +48,48 @@ ExchangeRates = List[ExchangeRate]
 
 
 class ExchangeRatesHistory:
-    _history: ExchangeRates = []
+    # _history: ExchangeRates = []
+    _path_file = os.path.join("history.json")
+    history_data = {}
+
+    @classmethod
+    def read_history_file(cls):
+        with open(cls._path_file, "r") as file:
+            cls.history_data = json.load(file)
+        return cls.history_data
 
     @classmethod
     def add(cls, instance: ExchangeRate) -> None:
-        """We woud like to add ExchangeRates instances if it is not last duplicated"""
+        """We would like to add ExchangeRates instances if it is not last duplicated"""
 
-        if not cls._history:
-            cls._history.append(instance)
-        elif cls._history[-1] != instance:
-            cls._history.append(instance)
+        with open(cls._path_file, "r") as file:
+            data = json.load(file)
+        if not data:
+            # cls._history.append(instance)
+            cls.save_to_file(instance)
+        elif data != asdict(instance):
+            # cls._history.append(instance)
+            cls.save_to_file(instance)
 
     @classmethod
     def as_dict(cls) -> dict:
         """Main representation interface"""
 
-        return {"results": [asdict(er) for er in cls._history]}
+        return {"results": [asdict(er) for er in cls.history_data]}
+
+    @classmethod
+    def save_to_file(cls, instance: ExchangeRate) -> None:
+        """Write to json file"""
+        with open(cls._path_file, "w") as file:
+            json.dump(asdict(instance), file, indent=2)
 
 
 def btc_usd(request):
     # NOTE: Connect to the external exchange rates API
-    API_KEY = "82I46WMYT3C7EX3J"
-    url_a = "https://www.alphavantage.co/"
+    API_KEY = os.getenv("API_KEY")
     url = (
-        f"{url_a}"
-        + f"query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={API_KEY}"
+        "https://www.alphavantage.co/"
+        f"query?function=CURRENCY_EXCHANGE_RATE&from_currency=BTC&to_currency=USD&apikey={API_KEY}"
     )
     response = requests.get(url)
 
