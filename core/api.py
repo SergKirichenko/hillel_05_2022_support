fe412cb6d4051d064b01acd6c08ada391cd54ede
Serg Kirichenko <ss86kirichenko@gmail.com>
2022-08-11 18:17:02 +0300

    django-project-1.2.6(core-serializ)

--- core/api.py
+++ core/api.py
@@ -8,34 +8,38 @@ from core.models import Ticket
 
 User = get_user_model()
 
-
-def user_as_dict(user: User) -> dict:
-    return {
-        "username": user.username,
-        "email": user.email,
-        "phone": user.phone,
-        "first_name": user.phone,
-        "last_name": user.phone,
-        "age": user.phone,
-    }
-
-
-def ticket_as_dict(ticket: Ticket) -> dict:
-    return {
-        "id": ticket.id,  # type: ignore
-        "theme": ticket.theme,
-        "description": ticket.description,
-        "operator": user_as_dict(ticket.operator),
-        "resolved": ticket.resolved,
-        "created_at": ticket.created_at,
-        "updated_at": ticket.updated_at,
-    }
+#
+# def user_as_dict(user: User) -> dict:
+#     return {
+#         "username": user.username,
+#         "email": user.email,
+#         "phone": user.phone,
+#         "first_name": user.phone,
+#         "last_name": user.phone,
+#         "age": user.phone,
+#     }
+#
+#
+# def ticket_as_dict(ticket: Ticket) -> dict:
+#     return {
+#         "id": ticket.id,  # type: ignore
+#         "theme": ticket.theme,
+#         "description": ticket.description,
+#         "operator": user_as_dict(ticket.operator),
+#         "resolved": ticket.resolved,
+#         "created_at": ticket.created_at,
+#         "updated_at": ticket.updated_at,
+#     }
 
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
-        fields = "__all__"
+        exclude = [
+            "created_at",
+            "updated_at",
+        ]
+        # fields = "__all__"
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -43,7 +47,20 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = "__all__"
+        exclude = [
+            "password",
+            "last_login",
+            "updated_at",
+            "is_active",
+            "is_staff",
+            "is_superuser",
+            "created_at",
+            "updateed_at",
+            "groups",
+            "user_permissions",
+        ]
+
+        # fields = "__all__"
 
 
 class TicketSerializer(serializers.ModelSerializer):
@@ -55,8 +72,35 @@ class TicketSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
+# class RoleLightSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Role
+#         fields = ["id", "name"]
+
+
+# class UserLightSerializer(serializers.ModelSerializer):
+#     role = RoleLightSerializer()
+
+#     class Meta:
+#         model = User
+#         fields = ["id", "username", "email", "role"]
+
+
+class TicketLightSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ticket
+        fields = ["id", "theme", "resolved", "operator", "client"]
+
+
 @api_view(["GET"])
 def get_all_tickets(request):
     tickets = Ticket.objects.all()
     data = TicketSerializer(tickets, many=True).data
+    return Response(data=data)
+
+
+@api_view(["GET"])
+def get_ticket(request, id_: int):
+    tickets = Ticket.objects.get(id=id_)
+    data = TicketSerializer(tickets).data
     return Response(data=data)
